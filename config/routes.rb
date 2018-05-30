b1d382f574f340a2c77b61d9a90ba3831ed7063d
alex <alkchen@gmail.com>
2018-05-30 00:44:23 -0700

    update gemfile and create organization and account

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :organizations
+  resources :accounts
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
