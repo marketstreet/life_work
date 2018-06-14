036826c344fef1f7504bc679ba0e5ea3923da523
Eric Chen <eric87chen@gmail.com>
2018-06-13 22:48:52 -0500

    Added models and schema

--- db/migrate/20180526060644_create_devise_users.rb
+++ db/migrate/20180526060644_create_devise_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class AddDeviseToUsers < ActiveRecord::Migration[5.1]
-  def self.up
-    change_table :users do |t|
+class CreateDeviseUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
@@ -40,11 +40,5 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-  end
-
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
   end
 end
